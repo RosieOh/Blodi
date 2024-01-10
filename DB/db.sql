c40b2fab67d15bd1e3095039421d24fbefee92df
RosieOh <20172207@gm.hannam.ac.kr>
2024-01-10 20:23:50 +0900

    📝build : Blodi 초기 빌드셋업, 안정성을 위해 2.7.1버전으로ㅗ 간다📝

---
+++ DB/db.sql
@@ -0,0 +1,3 @@
+create database blodi;
+
+use blodi;
---
+++ src/test/java/com/blodi/BlodiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blodi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlodiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
